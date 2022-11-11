a766e265522a2d94f424cdc22a0fb05dd1dfd4c5
Dumitru Motpan <dumitru.motpan@gmail.com>
2022-11-11 08:21:21 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TheFooter from './components/TheFooter';
+import TheControls from './components/TheControls';
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <the-controls/>
+  </main>
+
+  <the-footer />
+</template>
